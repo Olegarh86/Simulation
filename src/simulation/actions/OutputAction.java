8f665ff6d516a5ad6deace426e14690b5e478f66
Olegarh86 <malegik86@mail.ru>
2025-11-18 13:29:35 +0500

    refactor: add method 'save' to OutputAction to implement saving of all moves

--- src/simulation/actions/OutputAction.java
+++ src/simulation/actions/OutputAction.java
@@ -2,14 +2,20 @@ package simulation.actions;
 
 import simulation.Simulation;
 import simulation.config.Config;
+import simulation.entity.Entity;
 import simulation.view.output.Output;
 import simulation.view.renderer.BaseSimulationRenderer;
 import simulation.view.renderer.Renderer;
+import simulation.world.Coordinate;
 import simulation.world.WorldMap;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class OutputAction implements Action {
-    Output output;
-    Renderer renderer;
+    private final Output output;
+    private final Renderer renderer;
+    Map<Integer, Map<Coordinate, Entity>> countMoveStateWorld = new HashMap<>();
 
     public OutputAction(Output output) {
         this.output = output;
@@ -21,5 +27,10 @@ public class OutputAction implements Action {
         output.count(Simulation.countOfMoves++);
         renderer.draw(world, config);
         output.controls();
+        save(Simulation.countOfMoves, world);
+    }
+
+    private void save(Integer count, WorldMap world) {
+        countMoveStateWorld.put(count, world.getCoordinatesEntities());
     }
 }
